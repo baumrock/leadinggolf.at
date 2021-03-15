b79299c2aded2653115f23d381d3a8c12a864c1c
BernhardBaumrock <office@baumrock.com>
2021-03-15 18:45:33 +0100

    new config

--- site/config.php
+++ site/config.php
@@ -6,9 +6,18 @@ $config->chmodDir = '0755';
 $config->chmodFile = '0644';
 $config->timezone = 'Europe/Vienna';
 $config->httpHosts = array('www.leadinggolf.at');
+$config->installed = 1613307369;
 
 $config->googleTagManagerID = 'GTM-NZZSV9T';
 setlocale(LC_ALL,'de_AT.UTF-8', 'en_GB.UTF-8');
 
-// include config file from outside of the repo
-include('../config.php');
+// try to load live config
+$configs = [
+  "/home/users/leadinggolf/files/config-golf.php",
+  __DIR__."/config-local.php",
+];
+foreach($configs as $c) {
+  if(!is_file($c)) continue;
+  return include($c);
+}
+die("no config found");
